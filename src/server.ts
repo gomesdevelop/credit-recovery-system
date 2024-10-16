7470b3978016c9afb5e0db065413d6c3bd5c3027
Cleber Gomes <fclebinho@gmail.com>
2024-10-16 13:11:44 -0300

    fix: ajustes

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import {
   InstitutionResolver,
   WorkflowResolver,
 } from "./resolvers";
+import { GraphQLError } from "graphql";
 
 async function bootstrap() {
   const schema = await buildSchema({
@@ -27,7 +28,37 @@ async function bootstrap() {
 
   const server = new ApolloServer({ schema });
 
-  startStandaloneServer(server).then(({ url }) => {
+  const getUser = (token: string) => {
+    // get user from token
+    return token;
+  };
+
+  startStandaloneServer(server, {
+    // highlight-start
+    context: async ({ req }) => {
+      // get the user token from the headers
+      const token = req.headers.authorization || "";
+
+      // try to retrieve a user with the token
+      const user = getUser(token);
+
+      // optionally block the user
+      // we could also check user roles/permissions here
+      if (!user)
+        // throwing a `GraphQLError` here allows us to specify an HTTP status code,
+        // standard `Error`s will have a 500 status code by default
+        throw new GraphQLError("User is not authenticated", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+            http: { status: 401 },
+          },
+        });
+
+      // add the user to the context
+      return { user };
+    },
+    // highlight-end
+  }).then(({ url }) => {
     console.log(`🚀  Server ready at: ${url}`);
   });
 }
